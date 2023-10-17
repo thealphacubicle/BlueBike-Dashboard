2fcd59d2694a48b23bfec3c7482653b9dbc9d35b
Srihari <74083272+thealphacubicle@users.noreply.github.com>
2023-10-17 16:59:15 -0400

    Update app.py
    Signed-off-by: Srihari <74083272+thealphacubicle@users.noreply.github.com>

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ Runs the Dash dashboard interface
 DS3500 HW2
 Authors: Srihari Raman & Reema Sharma
 """
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, dcc, html, dash_table, Input, Output
 import plotly.express as px
 import pandas as pd
 import dash_bootstrap_components as dbc
@@ -13,10 +13,12 @@ import numpy as np
 import math
 import analysis
 
+
 # Load CSV data
 df = pd.read_csv('https://raw.githubusercontent.com/thealphacubicle/BlueBike-Dashboard/main/src/bluebike_updated.csv')
 
-# Create a Dash app with pre-defined CSS styles
+
+# Create a Dash app with bootstrap styles
 app = Dash(__name__, external_stylesheets=[dbc.themes.LUX])
 
 # Define the app layout
@@ -30,74 +32,50 @@ app.layout = dbc.Container([
         html.H2("Explore the Data:", style={'text-align': 'center', 'padding-top': '20px'}),
 
         # Select columns to explore dataset
-        dbc.Col([
+        dbc.Col(children=[
             dbc.Label('Pick a column to explore: ', style={'padding-bottom': '10px'}),
             dcc.Dropdown(id='columns-eda-dropdown',
-                         options=[{'label': col, 'value': col} for col in df.columns
-                                  if col in ['start_station_name', 'end_station_name', 'member_casual', 'distance',
-                                             'ride_duration_minutes']]
-                         ,
-                         value='member_casual', multi=False),
-
-            # Replace this with some other useful graph about chosen column
-            html.H2("Column Visualization: ",
-                    style={'padding-top': '20px', 'font-weight': 'bold', 'text-align': 'center'}),
-            dcc.Graph(id="column_statistics"),
+                         options=[{'label': col, 'value': col} for col in ['start_station_name', 'end_station_name',
+                                                                           'member_casual', 'distance',
+                                                                           'ride_duration_minutes']]
+                         , value='member_casual', multi=False),
 
         ]),
 
-        # Displaying statistics of selected column with indicators
-        html.H2("Key Metrics: ", style={'padding-top': '20px', 'font-weight': 'bold', 'text-align': 'center'}),
-        dbc.Row([
-            dbc.Col(dcc.Graph(figure=go.Figure(go.Indicator(
-                mode='number',
-                value=np.mean(list(df['distance'])),
-                number={"prefix": "", "suffix": " miles"},
-                title={'text': "Average Distance of Rides "})),
-            ), width=4),
-            dbc.Col(dcc.Graph(figure=go.Figure(go.Indicator(
-                mode='number',
-                value=np.mean(list(df['ride_duration_minutes'])),
-                number={"prefix": "", "suffix": " min"},
-                title={'text': "Average Ride Duration"})),
-            ), width=4),
-            dbc.Col(dcc.Graph(figure=go.Figure(go.Indicator(
-                mode='number',
-                value=np.mean(list(df['distance'] / df['ride_duration_minutes'])),
-                number={"prefix": "", "suffix": " mph"},
-                title={'text': "Average Speed of All Riders"})),
-            ), width=4)
-        ]),
+        # Replace this with some other useful graph about chosen column
+        html.H2("Column Visualization: ",
+                style={'padding-top': '20px', 'font-weight': 'bold', 'text-align': 'center'}),
+        dcc.Graph(id="column_statistics"),
 
-        html.Hr()
     ]),
 
-    # TODO: Format the radio items nicely
-    dbc.Row([
-        dbc.Col([
-            dbc.Form([
-                html.Div([
-                    dbc.Label("Pick a rider type to see ride starting areas:"),
-                    dbc.RadioItems(
-                        id='radio-ride-type',
-                        options=[
-                            {'label': 'All Riders', 'value': 'rideable_type'},
-                            {'label': 'Member', 'value': 'member'},
-                            {'label': 'Casual', 'value': 'casual'},  # Replace accordingly
-                        ],
-                        value='rideable_type')
-                ]),
-            ])
+    # Displaying statistics of selected column
+    html.H2("Key Metrics: ", style={'padding-top': '20px', 'font-weight': 'bold', 'text-align': 'center'}),
+    html.Div(
+        [
+            html.H4("Choose A Ride Group:"),
+            dbc.RadioItems(
+                options=[
+                    {"label": "Member", "value": 'member'},
+                    {"label": "Casual", "value": 'casual'},
+                ],
+                value='member',
+                id="radioitems-input",
+            ),
         ]),
-        dbc.Col([
-            dbc.Col(dcc.Graph(id='rides-map'), width=15)
-        ])
-    ])
+
+    # Add dynamic indicators for metrics
+    dbc.Row([
+        dbc.Col(dcc.Graph(id='mean_indicator'), width=4),
+        dbc.Col(dcc.Graph(id='ride_duration_indicator'), width=4),
+        dbc.Col(dcc.Graph(id='speed_avg_indicator'), width=4)
+    ]),
+
+    html.Hr()
 
 ], style={'backgroundColor': 'white', 'padding': '20px'}, fluid=True)
 
 
-########################################################################################################################
 @app.callback(Output("column_statistics", "figure"), Input('columns-eda-dropdown', 'value'))
 def viz_statistics_of_column(column):
     """
@@ -130,6 +108,11 @@ def viz_statistics_of_column(column):
     elif column == 'ride_duration_minutes':
         fig = analysis.create_violin_plot(df, 'ride_duration_minutes', 'member_casual', return_fig=True)
 
+    elif column == 'member_casual':
+        fig = px.scatter_mapbox(df, lat="start_lat", lon="start_lng", opacity=0.9, zoom=10,
+                                color='member_casual',
+                                title="Selection Points of All Rides", mapbox_style="open-street-map")
+
     # All other columns, just show value_counts by default
     else:
         fig = px.bar(
@@ -142,18 +125,42 @@ def viz_statistics_of_column(column):
     return fig
 
 
-@app.callback(Output('rides-map', 'figure'), Input('radio-ride-type', 'value'))
-def update_rides_map(user_type):
-    if user_type == 'rideable_type':
-        filtered_df = df
-    else:
-        filtered_df = df[df['member_casual'] == user_type]
+@app.callback(Output('mean_indicator', 'figure'), Input("radioitems-input", "value"))
+def mean_column_indicator(column):
+    filtered_df = df[df['member_casual'] == column]
 
-    fig = px.scatter_mapbox(filtered_df, lat="start_lat", lon="start_lng", opacity=0.9, zoom=10, color='member_casual',
-                            title="Selection Points of All Rides", mapbox_style="open-street-map")
+    figure = go.Figure(go.Indicator(
+        mode='number',
+        number={"prefix": "", "suffix": " miles"},
+        value=filtered_df['distance'].mean(),
+        title={'text': "Average Distance of Rides "}))
 
-    return fig
+    return figure
 
+
+@app.callback(Output('ride_duration_indicator', 'figure'), Input("radioitems-input", "value"))
+def ride_duration_indicator(column):
+    filtered_df = df[df['member_casual'] == column]
+
+    figure = go.Figure(go.Indicator(
+        mode='number',
+        number={"prefix": "", "suffix": " minutes"},
+        value=filtered_df['ride_duration_minutes'].mean(),
+        title={'text': "Average Ride Duration "}))
+
+    return figure
+
+
+@app.callback(Output('speed_avg_indicator', 'figure'), Input("radioitems-input", "value"))
+def speed_indicator(column):
+    filtered_df = df[df['member_casual'] == column]
+    figure = go.Figure(go.Indicator(
+        mode='number',
+        value=np.mean(list(filtered_df['distance'] / filtered_df['ride_duration_minutes'])),
+        number={"prefix": "", "suffix": " mph"},
+        title={'text': "Average Speed of All Riders"}))
+
+    return figure
 
 
 # Run the app
