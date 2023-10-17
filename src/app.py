d6c59230b045bfa0d8dd0d49db092512adff4bca
Srihari Raman <srihariraman9@gmail.com>
2023-10-17 16:57:03 -0400

    Finished dashboard components

--- src/app.py
+++ src/app.py
@@ -9,10 +9,14 @@ import pandas as pd
 import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
 import sankey as sk
+import numpy as np
+import math
+import analysis
+
 
 # Load CSV data
-df = pd.read_csv('https://raw.githubusercontent.com/thealphacubicle/'
-                 'BlueBike-Dashboard/main/src/bluebike_trunc.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/thealphacubicle/BlueBike-Dashboard/main/src/bluebike_updated.csv')
+
 
 # Create a Dash app with bootstrap styles
 app = Dash(__name__, external_stylesheets=[dbc.themes.LUX])
@@ -20,24 +24,8 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.LUX])
 # Define the app layout
 app.layout = dbc.Container([
 
-    # TODO: Change background color to white
-    # Dashboard navbar
-    dbc.NavbarSimple(
-        brand="Blue Bike Ridership Visualization",
-        color="blue",
-        dark=True,
-        id="nav-bar",
-        style={'background': 'black', 'color': 'white', 'text-align': 'center'},
-        brand_style={'fontSize': '40px'}),
-
-    # TODO: Add about section to project
-    html.Div([
-        html.H2("About the Project:", style={'text-align': 'center', 'padding-top': '20px'}),
-
-        # TODO: Edit description once everything else is done
-        html.P("TO BE CHANGED.", style={'text-align': 'center', 'padding-top': '15px'}),
-        html.Hr()
-    ]),
+    html.H1('Blue Bike Ridership Visualization', style={'textAlign': 'center', 'color': 'black',
+                                                        'backgroundColor': 'lightblue', 'padding': '10px'}),
 
     # First row
     dbc.Row([
@@ -47,71 +35,47 @@ app.layout = dbc.Container([
         dbc.Col(children=[
             dbc.Label('Pick a column to explore: ', style={'padding-bottom': '10px'}),
             dcc.Dropdown(id='columns-eda-dropdown',
-                         options=[{'label': col, 'value': col} for col in df.columns if 'id' not in col and 'lat' not in
-                                  col and 'lng' not in col]
-                         ,
-                         value=df.columns[-1], multi=False),
+                         options=[{'label': col, 'value': col} for col in ['start_station_name', 'end_station_name',
+                                                                           'member_casual', 'distance',
+                                                                           'ride_duration_minutes']]
+                         , value='member_casual', multi=False),
 
-            # Describing the selected column
-            dbc.Label("Description: ", style={'padding-top': '35px', 'font-weight': 'bold'}),
-            html.P(children="CREATE CALLBACK FUNCTION TO INSERT METADATA DESCRIPTION HERE!!!",
-                   style={'padding-top': '10px', 'padding-bottom': '15px'}, id="column_description_text"),
-
-
-            # TODO: Displaying statistics of selected column with indicators
-            html.H2("Statistics: ", style={'padding-top': '20px', 'font-weight': 'bold', 'text-align': 'center'}),
-            dbc.Row([
-                dbc.Col(dcc.Graph(figure=go.Figure(go.Indicator(
-                    mode='number',
-                    value=000,
-                    number={"prefix": "", "suffix": " miles"},
-                    title={'text': "Average Distance of Rides "})),
-                ), width=4),
-                dbc.Col(dcc.Graph(figure=go.Figure(go.Indicator(
-                    mode='number',
-                    value=000,
-                    number={"prefix": "", "suffix": " min"},
-                    title={'text': "Average Ride Duration"})),
-                ), width=4),
-                dbc.Col(dcc.Graph(figure=go.Figure(go.Indicator(
-                    mode='number',
-                    value=000,
-                    number={"prefix": "", "suffix": " mph"},
-                    title={'text': "Average Speed of All Riders"})),
-                ), width=4)
-            ]),
         ]),
 
-        # TODO: Replace this with some other useful graph about chosen column
+        # Replace this with some other useful graph about chosen column
+        html.H2("Column Visualization: ",
+                style={'padding-top': '20px', 'font-weight': 'bold', 'text-align': 'center'}),
         dcc.Graph(id="column_statistics"),
 
-        html.Hr()
     ]),
 
-    # Add two other useful graphs
+    # Displaying statistics of selected column
+    html.H2("Key Metrics: ", style={'padding-top': '20px', 'font-weight': 'bold', 'text-align': 'center'}),
+    html.Div(
+        [
+            html.H4("Choose A Ride Group:"),
+            dbc.RadioItems(
+                options=[
+                    {"label": "Member", "value": 'member'},
+                    {"label": "Casual", "value": 'casual'},
+                ],
+                value='member',
+                id="radioitems-input",
+            ),
+        ]),
+
+    # Add dynamic indicators for metrics
     dbc.Row([
-        # TODO: Edit this ride type graph -> it's redundant and useless
-        dbc.Col(dcc.Graph(id='ride-type-bar-chart'), width=6),
-        dbc.Col(dcc.Graph(id='rides-map'), width=6)
-    ])
+        dbc.Col(dcc.Graph(id='mean_indicator'), width=4),
+        dbc.Col(dcc.Graph(id='ride_duration_indicator'), width=4),
+        dbc.Col(dcc.Graph(id='speed_avg_indicator'), width=4)
+    ]),
+
+    html.Hr()
+
 ], style={'backgroundColor': 'white', 'padding': '20px'}, fluid=True)
 
 
-########################################################################################################################
-# TODO: CREATE CALLBACK FUNCTIONS FOR ALL DYNAMIC ELEMENTS
-
-# TODO: Create a metadata dictionary to map column to respective description
-@app.callback(Output("column_description_text", "children"), Input('columns-eda-dropdown', 'value'))
-def print_column_info(column):
-    """
-    Callback function to print the selected column's metadata
-    :param column: Column selected from dropdown menu
-    :return: Text about the column
-    """
-    return f"Replace with information about {column}!!!"
-
-
-# TODO: UPDATE IF ELSE WITH TYPE CHECK AND ACCORDING GRAPH DISPLAY
 @app.callback(Output("column_statistics", "figure"), Input('columns-eda-dropdown', 'value'))
 def viz_statistics_of_column(column):
     """
@@ -125,16 +89,8 @@ def viz_statistics_of_column(column):
     value_counts = df[column].value_counts()
     places = ['start_station_name', 'end_station_name']
 
-    # Displaying bar chart for columns that are numbers
-    if column not in ['started_at', 'ended_at', 'start_station_name', 'end_station_name']:
-        fig = px.bar(
-            data_frame=value_counts.reset_index(),
-            x=column,
-            y='count',
-            labels={'index': 'Category', 'column_name': 'Count'},
-            title=f'Visualizing Count:'
-        )
-
+    if column is None:
+        column = "member_casual"
     # Displaying sankey diagram for columns that are departure/arrival stations
     elif column in places:
         counts_df = sk.create_value_column(df, ['member_casual', column], 'count')
@@ -142,28 +98,69 @@ def viz_statistics_of_column(column):
         counts_df = counts_df[counts_df['count'] >= 850]
 
         fig = sk.make_sankey(counts_df, 'member_casual', column,
-                             vals=None, return_fig=True)
+                             vals=None, return_fig=True, title_text=f"Member Type to {column}")
 
-    # Figure out how to deal with started_at, ended_at columns
+    # Graph distance
+    elif column == 'distance':
+        fig = analysis.create_dist_kde(df, 'member_casual', column, return_fig=True)
+        fig.update_xaxes(range=[0, math.ceil(np.quantile(df[column], 0.75) * 2.25)])
+
+    elif column == 'ride_duration_minutes':
+        fig = analysis.create_violin_plot(df, 'ride_duration_minutes', 'member_casual', return_fig=True)
+
+    elif column == 'member_casual':
+        fig = px.scatter_mapbox(df, lat="start_lat", lon="start_lng", opacity=0.9, zoom=10,
+                                color='member_casual',
+                                title="Selection Points of All Rides", mapbox_style="open-street-map")
+
+    # All other columns, just show value_counts by default
     else:
-        fig = None
+        fig = px.bar(
+            data_frame=value_counts.reset_index(),
+            x=column,
+            y='count',
+            labels={'index': 'Category', 'column_name': 'Count'}
+        )
 
     return fig
 
-# TODO: Replace this function with KDE plot for distances
-@app.callback(Output('ride-type-bar-chart', 'figure'), [Input('ride-type-bar-chart', 'id')])
-def update_ride_type_bar_chart(_):
-    fig = px.bar(df, x=df['rideable_type'].value_counts().index, y=df['rideable_type'].value_counts().values,
-                 title="Ride Types")
-    return fig
+
+@app.callback(Output('mean_indicator', 'figure'), Input("radioitems-input", "value"))
+def mean_column_indicator(column):
+    filtered_df = df[df['member_casual'] == column]
+
+    figure = go.Figure(go.Indicator(
+        mode='number',
+        number={"prefix": "", "suffix": " miles"},
+        value=filtered_df['distance'].mean(),
+        title={'text': "Average Distance of Rides "}))
+
+    return figure
 
 
-@app.callback(Output('rides-map', 'figure'), [Input('rides-map', 'id')])
-def update_rides_map(_):
-    fig = px.scatter_mapbox(df, lat="start_lat", lon="start_lng", color="rideable_type",
-                            title="Selection Points of All Rides", mapbox_style="open-street-map")
+@app.callback(Output('ride_duration_indicator', 'figure'), Input("radioitems-input", "value"))
+def ride_duration_indicator(column):
+    filtered_df = df[df['member_casual'] == column]
 
-    return fig
+    figure = go.Figure(go.Indicator(
+        mode='number',
+        number={"prefix": "", "suffix": " minutes"},
+        value=filtered_df['ride_duration_minutes'].mean(),
+        title={'text': "Average Ride Duration "}))
+
+    return figure
+
+
+@app.callback(Output('speed_avg_indicator', 'figure'), Input("radioitems-input", "value"))
+def speed_indicator(column):
+    filtered_df = df[df['member_casual'] == column]
+    figure = go.Figure(go.Indicator(
+        mode='number',
+        value=np.mean(list(filtered_df['distance'] / filtered_df['ride_duration_minutes'])),
+        number={"prefix": "", "suffix": " mph"},
+        title={'text': "Average Speed of All Riders"}))
+
+    return figure
 
 
 # Run the app
